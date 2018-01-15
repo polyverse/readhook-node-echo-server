c56614ea17df14938b1247d3128c9036da10b1f5
rsundahl <roy@polyverse.io>
2018-01-15 10:06:47 -0800

    Simple run command.

--- run.sh
+++ run.sh
@@ -1,20 +1,2 @@
 #!/bin/bash
-SHORT_DESCRIPTION="Test for redhook of polyverse/node-echo-server"
-
-USAGE="$(cat <<EOF
-
-Usage: sh run.sh ${0##*/}
-
-$SHORT_DESCRIPTION
-
-EOF
-)"
-
-if [ $# -ne 0 ]; then
-  printf "$USAGE\n\n"
-  exit 1
-fi
-
-# Use this command to run
 docker run -it --rm --name redhook-node-echo-server -p 8080:8080 polyverse/redhook-node-echo-server
-#docker run -it --rm --entrypoint /bin/bash --name redhook-node-echo-server -p 8080:8080 polyverse/redhook-node-echo-server
